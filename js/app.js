61f511cfc6565ed29b036f472e0646693d65372d
Guillaume Benard <guillaume.benard0@gmail.com>
2022-12-26 20:01:53 +0100

    adding technologies fiels and more

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $("#toggle-menu,#1,#2,#3,#4,#5").click(function() {
 
     } else{
         $("p").css("color", "#AFABA0");
+
     }
   });
 
@@ -19,5 +20,37 @@ card.addEventListener( 'click', function() {
 $('.projet-container .projet-slides > img').click(function(){
   var $smallImages = $(this).attr('src');
   $('.big-screen > img').attr('src', $smallImages);
-  console.log('hey')
+})
+
+
+// selecteur de description en fonction de l'image cliquée
+// fais a la va vite mais ça marche 
+
+
+$('#img1').click(function(){
+  $('#desc1').css("display", "block");
+  $('#desc2').css("display", "none");
+  $('#desc3').css("display", "none");
+  $('#desc4').css("display", "none");
+})
+
+$('#img2').click(function(){
+  $('#desc1').css("display", "none");
+  $('#desc2').css("display", "block");
+  $('#desc3').css("display", "none");
+  $('#desc4').css("display", "none");
+})
+
+$('#img3').click(function(){
+  $('#desc1').css("display", "none");
+  $('#desc2').css("display", "none");
+  $('#desc3').css("display", "block");
+  $('#desc4').css("display", "none");
+})
+
+$('#img4').click(function(){
+  $('#desc1').css("display", "none");
+  $('#desc2').css("display", "none");
+  $('#desc3').css("display", "none");
+  $('#desc4').css("display", "block");
 })
